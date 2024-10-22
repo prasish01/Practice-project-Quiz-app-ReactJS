a15fd234dd7a3cda89910786801701b7ad469dfd
prasish01 <prasish01@gmail.com>
2024-10-22 14:12:38 +0545

    initial commit

---
+++ src/components/QuestionTimer.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+export default function QuestionTimer({ timeout, onTimeout, mode }) {
+  const [remainingTime, setRemainingTime] = useState(timeout);
+
+  useEffect(() => {
+    const timer = setTimeout(onTimeout, timeout);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [onTimeout, timeout]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRemainingTime((prev) => prev - 10);
+    }, 10);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return (
+    <progress
+      id="question-time"
+      max={timeout}
+      value={remainingTime}
+      className={mode}
+    />
+  );
+}
